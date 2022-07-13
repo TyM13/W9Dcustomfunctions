27701f6c0ca4c7ba27b8ada29b8da1679a81fb4b
Your Name <you@example.com>
2022-07-13 17:23:27 -0600

    finished part 2 of w9D

--- js/app.js
+++ js/app.js
@@ -1,5 +1,20 @@
-function math (number_one, number_two) {
-    let number_three = number_one + number_two;
-    return number_three *5;
+// function math (number_one, number_two) {
+//     let number_three = number_one + number_two;
+//     return number_three *5;
+// }
+// let result = math(2,3);
+// console.log(result);
+
+
+
+
+function word_length(words) {
+    if (words.length > 10 ) {
+        return true;
+    } else if (words.length <= 10) {
+        return false;
+    } else {
+        console.log(`we have an error`);
+    }
 }
-console.log(math(2, 3));
+word_length(`loremtrjg8`);
