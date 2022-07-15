a89b15f4719ef8cd099877982bf9f258ff65c3af
Your Name <you@example.com>
2022-07-14 20:53:49 -0600

    commented code on assignment

--- js/app.js
+++ js/app.js
@@ -14,7 +14,7 @@ console.log(result);
 
 
 
-function word_length(words) {    // //function, name of function,(paramaters, arguements)
+function word_length(words) {     //function, name of function,(paramaters, arguements)
     if (words.length > 10 ) {    // if statment tht if wordlength is less greater than 10 return true
         return true;
     } else if (words.length <= 10) {     // else statment tht if wordlength is less greater than 10 return true
@@ -35,11 +35,11 @@ word_length(`lor`);
 
 
 
-function starts(array_of_strings) {
-    for (let counter = 0; counter < array_of_strings.length; counter++) {
-        if (array_of_strings[counter].startsWith(`ph`)) {
+function starts(array_of_strings) {     //function, name of function,(paramaters, arguements)
+    for (let counter = 0; counter < array_of_strings.length; counter++) {       //for loop, counter sets index to 0, if counter is less than array of strings add 1 to index
+        if (array_of_strings[counter].startsWith(`ph`)) {       // accesses array with the number that counter is on and checks to see if it starts with ph
             console.log(array_of_strings[counter]);
-            return (array_of_strings[counter]);
+            return (array_of_strings[counter]);     // ends the loop when it finds index with ph
         }else{
             console.log(`error`);
         }
@@ -48,8 +48,8 @@ function starts(array_of_strings) {
 
 }
 
-starts([`pands`, `tretrer`, `phantasmil`, `low`]);
+starts([`pands`, `tretrer`, `phantasmil`, `low`]);      //calls name of function with arguement 3 different times
 
-starts([`pants`, `treats`, `phantasic`, `lowered`]);
+starts([`pants`, `phantasic`,  `treats`, `lowered`]);
 
 starts([`pan`, `tret`, `phast`, `lower`]);
